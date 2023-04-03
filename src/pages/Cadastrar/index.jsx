85c8cdcbe6bf8dd4bdc20a5e0ba5221e81fe86f1
Filipe Alves <lipeqa@gmail.com>
2023-04-03 09:07:28 -0300

    refactor:tods os dados do input em uma  estado só

--- src/pages/Cadastrar/index.jsx
+++ src/pages/Cadastrar/index.jsx
@@ -1,4 +1,3 @@
-
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
@@ -8,184 +7,143 @@ import "./style.jsx";
 import { ContainStyled, FormStyled, BtnStyled } from "./style.jsx";
 
 export const Cadastro = () => {
-  const [nome, setNome] = useState("");
-  const [sobrenome, setSobrenome] = useState("");
-  const [email, setEmail] = useState("");
-  const [telefone, setTelefone] = useState("");
-  const [cep, setCep] = useState("");
-  const [rua, setRua] = useState("");
-  const [numero, setNumero] = useState("");
-  const [bairro, setBairro] = useState("");
-  const [dataNascimento, setDataNascimento] = useState("");
-  const [cpf, setCpf] = useState("");
-  const [salario, setSalario] = useState("");
-  const [cargo, setCargo] = useState("");
-
+  const [user, setUser] = useState({
+    nome: "",
+    sobrenome: "",
+    email: "",
+    telefone: "",
+    dataNascimento: "",
+    cep: "",
+    rua: "",
+    numero: "",
+    bairro: "",
+    cpf: "",
+    salario: "",
+    cargo: "",
+  });
+  const handleInputChange = (fild) => {
+    return (e) => {
+      setUser((prev) => ({
+        ...prev,
+        [fild]: e.target.value,
+      }));
+    };
+  };
   const { handleInformacao } = useContext(CustomerContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
     handleInformacao({
-      nome,
-      sobrenome,
-      email,
-      telefone,
-      dataNascimento,
-      cep,
-      rua,
-      numero,
-      bairro,
-      cpf,
-      salario,
-      cargo,
+      ...user,
     });
-    setNome("");
-    setSobrenome("");
-    setEmail("");
-    setTelefone("");
-    setCep("");
-    setRua("");
-    setRua("");
-    setNumero("");
-    setBairro("");
-    setDataNascimento("");
-    setCpf("");
-    setSalario("");
-    setCargo("");
   };
   return (
-  <ContainStyled>
-  <FormStyled>
-    <h3>Cadastro de Funcionário</h3>
-    <form action="">
-      <span>
-        <Input
-          required
-          type="text"
-          name="nome"
-          value={nome}
-          onChange={(e) => {
-            setNome(e.target.value);
-          }}
-        />
-      </span>
-      <span>
-        <Input
-          type="text"
-          name="sobrenome"
-          value={sobrenome}
-          onChange={(e) => {
-            setSobrenome(e.target.value);
-          }}
-        />
-      </span>
-      <span>
-        <Input
-          type="email"
-          name="email"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-      </span>
-      <span>
-        <Input
-          type="tel"
-          name="telefone"
-          value={telefone}
-          onChange={(e) => {
-            setTelefone(e.target.value);
-          }}
-        />
-      </span>
-      <span>
-        <Input
-          type="text"
-          name="cep"
-          value={cep}
-          onChange={(e) => {
-            setCep(e.target.value);
-          }}
-        />
-      </span>
-      <span>
-        <Input
-          type="text"
-          name="rua"
-          value={rua}
-          onChange={(e) => {
-            setRua(e.target.value);
-          }}
-        />
-      </span>
-      <span>
-        <Input
-          type="text"
-          name="numero"
-          value={numero}
-          onChange={(e) => {
-            setNumero(e.target.value);
-          }}
-        />
-      </span>
-      <span>
-        <Input
-          type="text"
-          name="bairro"
-          value={bairro}
-          onChange={(e) => {
-            setBairro(e.target.value);
-          }}
-        />
-      </span>
-      <span>
-        <Input
-          type="text"
-          name="Data Nascimento"
-          value={dataNascimento}
-          onChange={(e) => {
-            setDataNascimento(e.target.value);
-          }}
-        />
-      </span>
-      <span>
-        <Input
-          type="text"
-          name="cpf"
-          value={cpf}
-          onChange={(e) => {
-            setCpf(e.target.value);
-          }}
-        />
-      </span>
-      <span>
-        <Input
-          type="text"
-          name="Salario Mensal"
-          value={salario}
-          onChange={(e) => {
-            setSalario(e.target.value);
-          }}
-        />
-      </span>
-      <span>
-        <Input
-          type="text"
-          name="cargo"
-          value={cargo}
-          onChange={(e) => {
-            setCargo(e.target.value);
-          }}
-        />
-      </span>
-      <BtnStyled>
-        <Button name={"salvar"} onClick={handleSubmit} type="submit" />
-      </BtnStyled>
-    </form>
-  </FormStyled>
-</ContainStyled>
-);
-  //return <Form />;
+    <ContainStyled>
+      <FormStyled>
+        <h3>Cadastro de Funcionário</h3>
+        <form action="">
+          <span>
+            <Input
+              required
+              type="text"
+              name="nome"
+              value={user.nome}
+              onChange={handleInputChange("nome")}
+            />
+          </span>
+          <span>
+            <Input
+              type="text"
+              name="sobrenome"
+              value={user.sobrenome}
+              onChange={handleInputChange("sobrenome")}
+            />
+          </span>
+          <span>
+            <Input
+              type="email"
+              name="email"
+              value={user.email}
+              onChange={handleInputChange("email")}
+            />
+          </span>
+          <span>
+            <Input
+              type="tel"
+              name="telefone"
+              value={user.telefone}
+              onChange={handleInputChange("telefone")}
+            />
+          </span>
+          <span>
+            <Input
+              type="text"
+              name="cep"
+              value={user.cep}
+              onChange={handleInputChange("cep")}
+            />
+          </span>
+          <span>
+            <Input
+              type="text"
+              name="rua"
+              value={user.rua}
+              onChange={handleInputChange("rua")}
+            />
+          </span>
+          <span>
+            <Input
+              type="text"
+              name="numero"
+              value={user.numero}
+              onChange={handleInputChange("numero")}
+            />
+          </span>
+          <span>
+            <Input
+              type="text"
+              name="bairro"
+              value={user.bairro}
+              onChange={handleInputChange("bairro")}
+            />
+          </span>
+          <span>
+            <Input
+              type="text"
+              name="Data Nascimento"
+              value={user.dataNascimento}
+              onChange={handleInputChange("dataNascimento")}
+            />
+          </span>
+          <span>
+            <Input
+              type="text"
+              name="cpf"
+              value={user.cpf}
+              onChange={handleInputChange("cpf")}
+            />
+          </span>
+          <span>
+            <Input
+              type="text"
+              name="Salario Mensal"
+              value={user.salario}
+              onChange={handleInputChange("salario")}
+            />
+          </span>
+          <span>
+            <Input
+              type="text"
+              name="cargo"
+              value={user.cargo}
+              onChange={handleInputChange("cargo")}
+            />
+          </span>
+          <BtnStyled>
+            <Button name={"salvar"} onClick={handleSubmit} type="submit" />
+          </BtnStyled>
+        </form>
+      </FormStyled>
+    </ContainStyled>
+  );
 };
